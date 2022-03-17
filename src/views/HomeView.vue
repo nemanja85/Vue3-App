31395846538acc19da5a41d769fef826b5715b3a
nemanja85 <mitic@deutsche-teilkauf.de>
2022-03-17 12:38:15 +0100

    Made API to use data from PokeAPI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,9 +6,25 @@
 
 <script>
 // @ is an alias to /src
+import { reactive } from 'vue';
 
 export default {
   name: 'HomeView',
-  components: {},
+  setup() {
+    const state = reactive({
+      poke: [],
+      urlID: {},
+    });
+
+    fetch('https://pokeapi.co/api/v2/pokemon?offset=0')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        state.poke = data.results;
+        state.urlID = data.results.reduce((acc, val, index) => {
+          acc = { ...acc, [val.name]: index + 1 };
+        });
+      });
+  },
 };
 </script>
