7b7b0e1a9f00e6f68edcb29fb6c0da9ff817d611
nemanja85 <mitic@deutsche-teilkauf.de>
2022-03-17 13:27:13 +0100

    Finished filtering API data with text field

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <h2>Poke API</h2>
     <input type="text" placeholder="Enter Poke name" class="form-input" v-model="content" />
     <div class="pokes">
-      <div v-for="(poke, index) in pokes" :key="index">
+      <div v-for="(poke, index) in filteredList" :key="index">
         <span>{{ poke.name }}</span>
       </div>
     </div>
@@ -24,7 +24,9 @@ export default {
       filteredList: computed(() => updatePokes()),
     });
 
-    function updatePokes() {}
+    function updatePokes() {
+      return !state.content ? [] : state.pokes.filter((poke) => poke.name.includes(state.content));
+    }
 
     fetch('https://pokeapi.co/api/v2/pokemon?offset=0')
       .then((res) => res.json())
